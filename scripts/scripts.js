c7119c01b7698369fe3a53741343cfcb33f13914
enjoythehummus <82919616+enjoythehummus@users.noreply.github.com>
2021-05-04 20:06:33 -0400

    General Improvements
    Made improvements to blog area, added footer at bottom with links, pinned navbar and side bar as site is scrolled
    
    Need to align nav-bar and center-container, allow for blog posts to be added/edited, add elements to individual pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
-const sponsorTile = document.getElementById('sponsorTile')
+// Sponsor Carousel
+
+const sponsorTile = document.getElementById('sponsor-tile');
 
 const sponsorImages = [
     'images/sponsors/ansley_real_estate.jpg',
@@ -19,7 +21,7 @@ const sponsorLinks = [
 ];
 
 var i = 0;
-var timeout = setTimeout(right, 2000);
+var timeout = setTimeout(right, 4000);
 
 function right() {
     i = (i + 1) % sponsorImages.length;
@@ -29,9 +31,59 @@ function right() {
 }
 
 function changeImage() {
-    sponsorTile.innerHTML = '';
+    sponsorTile.innerHTML = '<p>Our sponsors: </p>';
     sponsorTile.innerHTML += '<a href=' + sponsorLinks[i] + '> <img src=' + sponsorImages[i] + '> </a>';
-    timeout = setTimeout(right, 2000);
+    timeout = setTimeout(right, 4000);
 }
 
 changeImage();
+
+
+// Cookies for page location
+
+function getCookie(cName) {
+    var name = cName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function checkCookie() {
+    var user = getCookie("username");
+    if(user != "" && user != null) {
+        // start webpage at header element
+    } else {
+        setCookie("username", randomCookie(), )
+    }
+}
+
+function randomCookie() {
+  return (Math.random() * 100) * (Math.random() * 10) + (Math.random() * 100);
+}
+
+
+// Pin nav to top, extend, show walton fast pitch and logo  left side
+const body = document.querySelector('body');
+const nav = document.querySelector('.nav-bar');
+const navOffsetTop = nav.navOffsetTop;
+
+function fixNav() {
+  if(window.scrollY >= navOffsetTop) {
+      body.classList.add('fixed-nav');
+      body.style.paddingTop = nav.offsetHeight + 'px';
+  } else {
+      body.classList.remove('fixed-nav');
+      body.style.paddingTop = 0;
+  }
+}
+
+window.addEventListener('scroll', fixNav)
